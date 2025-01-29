da730f9e3d2e5497530e5bb143ea05f121ad1ff8
amiraliikh83 <amiraliikh83@gmail.com>
2025-01-29 19:01:22 +0330

    added folder structure and controller

---
+++ Controller/urlController.js
@@ -0,0 +1,23 @@
+const UrlService = require('../services/urlService');
+
+// ایجاد کوتاه‌کننده لینک
+exports.shortenUrl = async (req, res) => {
+  const { originalUrl } = req.body;
+  try {
+    const shortUrl = await UrlService.shortenUrl(originalUrl);
+    res.json({ shortUrl: `http://localhost:3000/${shortUrl}` });
+  } catch (error) {
+    res.status(500).send('Error shortening URL');
+  }
+};
+
+// دریافت آمار یک لینک کوتاه‌شده
+exports.getUrlStats = async (req, res) => {
+  const { shortUrl } = req.params;
+  try {
+    const stats = await UrlService.getUrlStats(shortUrl);
+    res.json(stats);
+  } catch (error) {
+    res.status(404).send('URL not found');
+  }
+};
