160d990be5bbf4bd5a2913f9b98a155637247328
amiraliikh83 <amiraliikh83@gmail.com>
2025-01-31 15:16:11 +0330

    Cleaning up extra parts

--- Controller/urlController.js
+++ Controller/urlController.js
@@ -1,23 +1,21 @@
-const UrlService = require('../Service/urlService');
+const UrlService = require("../Service/urlService");
 
-// ایجاد کوتاه‌کننده لینک
 exports.shortenUrl = async (req, res) => {
   const { originalUrl } = req.body;
   try {
     const shortUrl = await UrlService.shortenUrl(originalUrl);
     res.json({ shortUrl: `http://localhost:3000/${shortUrl}` });
   } catch (error) {
-    res.status(500).send('Error shortening URL');
+    res.status(500).send("Error shortening URL");
   }
 };
 
-// دریافت آمار یک لینک کوتاه‌شده
 exports.getUrlStats = async (req, res) => {
   const { shortUrl } = req.params;
   try {
     const stats = await UrlService.getUrlStats(shortUrl);
     res.json(stats);
   } catch (error) {
-    res.status(404).send('URL not found');
+    res.status(404).send("URL not found");
   }
 };
--- Model/url.js
+++ Model/url.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const shortid = require("shortid");
 
 const urlSchema = new mongoose.Schema({
   originalUrl: { type: String, required: true },
